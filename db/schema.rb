7bf85305aa693a6cb0afc36a7ee9d54819198284
Fernando Mejia Laguna <mejia190899@gmail.com>
2024-12-12 19:12:00 -0600

    Added complement of 'has_many' relationship from user to tasks; Ran 'rails db:reset' to fix index problems with user; ran 'db:migrate'.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_12_09_174410) do
+ActiveRecord::Schema[7.2].define(version: 2024_12_13_010604) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -25,7 +25,9 @@ ActiveRecord::Schema[7.2].define(version: 2024_12_09_174410) do
     t.integer "category_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "owner_id", null: false
     t.index ["category_id"], name: "index_tasks_on_category_id"
+    t.index ["owner_id"], name: "index_tasks_on_owner_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -41,4 +43,5 @@ ActiveRecord::Schema[7.2].define(version: 2024_12_09_174410) do
   end
 
   add_foreign_key "tasks", "categories"
+  add_foreign_key "tasks", "users", column: "owner_id"
 end
